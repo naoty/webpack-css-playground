921bc89fba0848b093db5b9364fedca56cace03a
Naoto Kaneko <naoty.k@gmail.com>
2017-08-18 01:11:50 +0900

    Add main.css to bundle multi CSS files into bundle.css

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,10 @@ const path = require("path");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-  entry: ["./src/stylesheets/layout.css"],
+  entry: [
+    "./src/stylesheets/layout.css",
+    "./src/stylesheets/main.css"
+  ],
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist", "javascripts")
