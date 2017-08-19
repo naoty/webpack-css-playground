6ac406521c10b1465fb1851473949774cb5ad244
Naoto Kaneko <naoty.k@gmail.com>
2017-08-19 20:07:46 +0900

    Add index.css to import CSS

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,7 @@ const path = require("path");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-  entry: [
-    "./src/stylesheets/layout.css",
-    "./src/stylesheets/main.css"
-  ],
+  entry: "./src/stylesheets/index.css",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist", "javascripts")
